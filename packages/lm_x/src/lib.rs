2a1b8d564d24d9d116b7481582353d29bf7dded9
noah <noah@gmail.com>
2025-12-30 21:13:35 +0800

    init

--- packages/lm_x/src/lib.rs
+++ packages/lm_x/src/lib.rs
@@ -6,3 +6,12 @@ use napi_derive::napi;
 pub fn plus_100(input: u32) -> u32 {
   input + 100
 }
+
+#[napi]
+pub fn hello(name: Option<String>, input: Option<i32>) -> String {
+  format!(
+    "hello {}, input is {}",
+    name.unwrap_or("default".into()),
+    input.unwrap_or(999)
+  )
+}
